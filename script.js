a360c3dae59fb1ab1759c05558cc1f4569743202
Wolvrin12 <anouarchlih31@gmail.com>
2023-08-23 18:01:48 +0100

    Adding scroll trigger

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const header = document.getElementsByTagName("nav");
 const tabs = document.getElementsByTagName("li");
 
 for (let i = 0; i < tabs.length; i++) {
@@ -7,3 +8,15 @@ for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.add("selected");
   });
 }
+
+ScrollTrigger.create({
+  trigger: header,
+  start: "top center",
+  onUpdate: (self) => {
+    if (self.progress <= 0.85) {
+      header[0].style.background = "#e9ecef";
+    } else {
+      header[0].style.background = "#1b2834";
+    }
+  },
+});
