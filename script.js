837f4ba3ab41b8a55bcc20a085a49459cc3f1d0f
Wolvrin12 <anouarchlih31@gmail.com>
2023-08-23 19:06:11 +0100

    Making styles change at scroll

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-const header = document.getElementsByTagName("nav");
+const header = document.getElementsByTagName("nav")[0];
+const logo = header.children[0].children[0];
 const tabs = document.getElementsByTagName("li");
+
+const useUpStyles = () => {
+  let selected_tab = document.getElementsByClassName("selected")[0];
+  header.style.background = "#e9ecef";
+  logo.style.color = "#113b3b";
+  selected_tab.style.background = "#cbced2";
+};
+
+const useDownStyles = () => {
+  let selected_tab = document.getElementsByClassName("selected")[0];
+  header.style.background = "#1b2834";
+  logo.style.color = "#979bdb";
+  selected_tab.style.background = "#1a092f";
+};
 
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", () => {
     let selected = document.getElementsByClassName("selected")[0];
     selected.classList.remove("selected");
+    selected.style.background = "none";
     tabs[i].classList.add("selected");
+    useUpStyles();
   });
 }
 
 ScrollTrigger.create({
-  trigger: header,
-  start: "top center",
+  start: "top",
   onUpdate: (self) => {
     if (self.progress <= 0.85) {
-      header[0].style.background = "#e9ecef";
+      useUpStyles();
     } else {
-      header[0].style.background = "#1b2834";
+      useDownStyles();
     }
   },
 });
